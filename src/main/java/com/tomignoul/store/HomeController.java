28dc3d579fbb4af3cc1c10d9423ae4a04ffee7b8
Tom Ignoul <tom.ignoul@student.ehb.be>
2025-06-21 09:13:31 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/tomignoul/store/HomeController.java
@@ -0,0 +1,23 @@
+package com.tomignoul.store;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @Value("${spring.application.name}")
+    private String appName;
+
+
+    @RequestMapping("/")
+    public String index(){
+        System.out.println("appName: " + appName);
+        String viewName = getViewName();
+        return viewName;
+    }
+
+    private String getViewName(){
+        return "index.html";
+    }
+
+}
