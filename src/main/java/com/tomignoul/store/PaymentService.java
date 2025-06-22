677e3feba673a3b5d0ef6688e9bd0a0ecd0fd056
Tom Ignoul <tom.ignoul@student.ehb.be>
2025-06-22 19:10:51 +0200

    Feat: added PaymentServices via constructor Injection

---
+++ src/main/java/com/tomignoul/store/PaymentService.java
@@ -0,0 +1,5 @@
+package com.tomignoul.store;
+
+public interface PaymentService {
+    void processPayment(double amount);
+}
--- src/main/java/com/tomignoul/store/StoreApplication.java
+++ src/main/java/com/tomignoul/store/StoreApplication.java
@@ -1,13 +1,17 @@
 package com.tomignoul.store;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.annotation.Order;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StoreApplication.class, args);
+//        SpringApplication.run(StoreApplication.class, args);
+//        PaymentService paymentService = new StripePaymentService();
+//        OrderService orderService = new OrderService(paymentService)
+        var orderService = new OrderService(new PayPalPaymentService());
+        orderService.placeOrder();
     }
 
 }
