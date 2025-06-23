7d003128d928900e29f5a20ba3381c2d69c4d14f
Tom Ignoul <tom.ignoul@student.ehb.be>
2025-06-23 22:11:52 +0200

    Added Embeddable to Address

---
+++ src/main/java/com/tomignoul/store/model/Address.java
@@ -0,0 +1,51 @@
+package com.tomignoul.store.model;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+    private String street;
+    private String city;
+    private String number;
+    private String postalCode;
+    private String country;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
