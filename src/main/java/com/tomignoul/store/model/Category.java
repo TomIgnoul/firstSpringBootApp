5af9c0fb22db7e384cddf5d8d11844861ccced6f
Tom Ignoul <tom.ignoul@student.ehb.be>
2025-06-25 22:33:21 +0200

    Feat: made two Entities (Product & Category) with Hibernate and JPA

---
+++ src/main/java/com/tomignoul/store/model/Category.java
@@ -0,0 +1,38 @@
+package com.tomignoul.store.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String categoryName;
+
+    //constructors
+    public Category(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Category(){};
+
+    //Getters & Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
---
+++ src/main/java/com/tomignoul/store/model/Product.java
@@ -0,0 +1,97 @@
+package com.tomignoul.store.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+
+import java.math.BigDecimal;
+
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long productId;
+    private String description;
+    private BigDecimal price;
+    private double quantity;
+    private String img;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+    //constructors
+    public Product(long productId, String description, BigDecimal price, double quantity, String img, User user, Category category) {
+        this.productId = productId;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        this.img = img;
+        this.user = user;
+        this.category = category;
+    }
+    public Product (){};
+
+    // Getters & Setters
+
+    public long getproductId() {
+        return productId;
+    }
+
+    public void setproductId(long productId) {
+        this.productId = productId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
